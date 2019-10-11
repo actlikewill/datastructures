fdd41a542f2e1dd91a07762ea5b8c5a49f4bbdb0
actlikewill <wilsongaturu@gmail.com>
2019-10-11 13:09:02 +0300

    server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = 3000;
 const LinkedList = require('./dataStructures/SinglyLinkedList');
 const DoublyLinkedList = require('./dataStructures/DoublyLinkedList');
 const Queue = require('./dataStructures/Queue');
+const BST = require('./dataStructures/BST');
+const BinaryHeaps = require('./dataStructures/BinaryHeaps');
+const PriorityQueue = require('./dataStructures/PriorityQueue');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
